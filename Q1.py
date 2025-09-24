3d79da45a2789c4aaa6746d197434f198420b16f
Rohan <ronnybob17@gmail.com>
2025-09-24 23:47:11 +0530

    Q1

---
+++ Q1.py
@@ -0,0 +1,6 @@
+def triple_and(p1, p2, p3):
+  return p1 and p2 and p3
+p1=input("Enter first parameter: ")
+p2=input("Enter second parameter: ")
+p3=input("Enter third parameter: ")
+print (triple_and(p1, p2, p3))
